ede4c014a7ab004af68d84ad00939986c59b8ca2
D.P <dp.bastings@posteo.net>
2023-05-04 16:18:23 +0200

    Provided function documentation.

--- include/ft_ctype.h
+++ include/ft_ctype.h
@@ -13,14 +13,53 @@
 #ifndef FT_CTYPE_H
 # define FT_CTYPE_H
 
+/* ft_isalnum -	Test whether a character is alphanumeric.
+ * @param c	The character to be tested.
+ * @return	0 if false, 1 if true.
+ */
 int	ft_isalnum(int c);
+
+/* ft_isalpha -	Test whether a character is a letter.
+ * @param c	The character to be tested.
+ * @return	0 if false, 1 if true.
+ */
 int	ft_isalpha(int c);
+
+/* ft_isascii -	Test whether a character is a valid ASCII value.
+ * @param c	The character to be tested.
+ * @return	0 if false, 1 if true.
+ */
 int	ft_isascii(int c);
+
+/* ft_isdigit -	Test whether a character is a decimal digit.
+ * @param c	The character to be tested.
+ * @return	0 if false, 1 if true.
+ */
 int	ft_isdigit(int c);
+
+/* ft_isprint -	Test whether a character is printable.
+ * @param c	The character to be tested.
+ * @return	0 if false, 1 if true.
+ */
 int	ft_isprint(int c);
+
+/* ft_isspace -	Test whether a character is whitespace.
+ * @param c	The character to be tested.
+ * @return	0 if false, 1 if true.
+ */
 int	ft_isspace(int c);
 
+
+/* ft_tolower -	Convert an uppercase letter to lowercase.
+ * @param c	The character to be converted.
+ * @return	The converted character.
+ */
 int	ft_tolower(int c);
+
+/* ft_toupper -	Convert a lowercase letter to uppercase.
+ * @param c	The character to be converted.
+ * @return	The converted character.
+ */
 int	ft_toupper(int c);
 
 #endif
